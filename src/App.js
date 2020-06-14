ab895a6c8cd53203eca2be01bc4d769ec4370f54
Abdulrafayzahid <zahidabdulrafay@gmail.com>
2020-06-14 16:45:21 +0500

    Add some basic React stuff

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({name}) {
+
+  const addFirstValue = (e) => {
+    document.getElementById("getFirstNum").innerText = e.target.value
+    let Ans = (document.querySelector('#firstInput').value - document.querySelector('#secondInput').value)
+
+    document.getElementById('asnwer').innerText = Ans
+  }
+
+  const addSecondValue = (e) => {
+    document.getElementById("getSecondNum").innerText = e.target.value
+    let Ans = document.querySelector('#firstInput').value - document.querySelector('#secondInput').value
+
+    document.getElementById('asnwer').innerText = Ans
+  }
+
+  let rest = () => {
+    document.querySelector('#getFirstNum').innerText = 0
+    document.querySelector('#getSecondNum').innerText = 0
+    document.querySelector('#asnwer').innerText = 0
+    document.querySelector('#firstInput').value = null
+    document.querySelector('#secondInput').value = null
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <h6>Hello World</h6>
-      </header>
+      <h1>Hello world from <span> {name} </span></h1>
+
+      <h2>Let's do some maths</h2>
+
+      <input type="number" id="firstInput"  onChange={(e) => addFirstValue(e)} />
+      <span> - </span> 
+      <input type="number" id="secondInput" onChange={(e) => addSecondValue(e)} />
+
+      <h3><span id="getFirstNum">0</span> - <span id="getSecondNum">0</span></h3>
+
+      <h3>Your answer is: <span id="asnwer"></span></h3>
+
+      <p>
+        <button onClick={rest}>Reset</button>
+      </p>
     </div>
   );
 }
